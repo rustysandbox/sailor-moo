1d78876399ea384e2b606c22030a695753c76ef0
sapanapoudel <sapana.poudel17@outlook.com>
2019-07-22 16:36:42 -0700

    mock data

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <h1>Sacred-Toast</h1>
+          <h2>Anime News Aggregator</h2>
+        </header>
+      </React.Fragment>
+    );
+  }
+}
