7f0af261a2d560c56fe9540701c33887df9d9d79
sapanapoudel <sapana.poudel17@outlook.com>
2019-07-23 11:08:56 -0700

    Add a link of about us page to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import AboutUs from './About-us';
 
-export default class Footer extends React.Component {
+
+export default class Footer extends Component{
   render() {
     return (
-      <React.Fragment>
-        
-      </React.Fragment>
+      <Fragment>
+        <BrowserRouter>
+        <Link to="/About-us">About US</Link>
+        <Route exact path="/About-us" component={AboutUs}></Route>
+        </BrowserRouter>
+
+      </Fragment>
     );
   }
 }
