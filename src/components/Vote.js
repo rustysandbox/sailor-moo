e5075167a17c5f440f0e9c3013c76e496d2fd887
bomibear <bomibearly@gmail.com>
2019-07-24 13:16:39 -0700

    remove decrement functionality

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,36 +1,28 @@
-import React, { Component, Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 
 class Vote extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-      currentNumberOfVote: "0" 
-    }
+    this.state = {
+      currentNumberOfVote: "0"
+    };
   }
 
   increaseVote = () => {
     let valueToModify = this.state.currentNumberOfVote;
     let modifiedValue = parseInt(valueToModify, 10) + 1;
-    this.setState({currentNumberOfVote: modifiedValue});
-  }
-
-  decreaseVote = () => {
-    let valueToModify = this.state.currentNumberOfVote;
-    let modifiedValue = parseInt(valueToModify, 10) - 1;
-    this.setState({currentNumberOfVote: modifiedValue});
-  }
-  render() { 
-    return ( 
+    this.setState({ currentNumberOfVote: modifiedValue });
+  };
+  render() {
+    return (
       <Fragment>
-        <section> 
+        <section>
           <h3>{this.state.currentNumberOfVote}</h3>
-
           <button onClick={this.increaseVote}>Like</button>
-          <button onClick={this.decreaseVote}>Dislike</button>
         </section>
       </Fragment>
-     );
+    );
   }
 }
- 
+
 export default Vote;
