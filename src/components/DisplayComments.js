a8623979cadac33471a5dbb44f25e608c16db217
sapanapoudel <sapana.poudel17@outlook.com>
2019-07-24 10:35:40 -0700

    generate avatar

--- src/components/DisplayComments.js
+++ src/components/DisplayComments.js
@@ -1,14 +1,20 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState, useEffect } from "react";
+
 
 class DisplayComments extends Component {
   constructor(props) {
     super(props);
+  }
+
+  getRandomInt() {
+    return Math.floor(Math.random() * Math.floor(1000));
   }
   render() {
     console.log("display ", this.props);
     return (
       <Fragment>
         <ul>
+          <img src={`https://api.adorable.io/avatars/90/${this.getRandomInt()}.png`}/>
           <li>{this.props.comment}</li>
         </ul>
       </Fragment>
