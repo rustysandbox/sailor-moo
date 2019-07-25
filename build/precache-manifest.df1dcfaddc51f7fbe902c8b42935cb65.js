c490917529c524c162e38ae8634565784b539b9c
sapanapoudel <sapana.poudel17@outlook.com>
2019-07-25 11:35:59 -0700

    deploy

---
+++ build/precache-manifest.df1dcfaddc51f7fbe902c8b42935cb65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0d0ed536c5d80465be31de3f475a35",
+    "url": "/sailor-moo/index.html"
+  },
+  {
+    "revision": "199a95f8e03c3d0027e0",
+    "url": "/sailor-moo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bea3612a614725f04108",
+    "url": "/sailor-moo/static/css/main.abe86555.chunk.css"
+  },
+  {
+    "revision": "199a95f8e03c3d0027e0",
+    "url": "/sailor-moo/static/js/2.0e51e978.chunk.js"
+  },
+  {
+    "revision": "bea3612a614725f04108",
+    "url": "/sailor-moo/static/js/main.6c854c84.chunk.js"
+  },
+  {
+    "revision": "5d609123270b9402f9cf",
+    "url": "/sailor-moo/static/js/runtime~main.5386f9cc.js"
+  },
+  {
+    "revision": "60707ce46a04cb14da8ffe13963d2826",
+    "url": "/sailor-moo/static/media/thumbsUp.60707ce4.png"
+  }
+]);
