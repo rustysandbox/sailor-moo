bda50cd6534ec23e16d9ad24abdec0df9a7f0fae
bomibear <bomibearly@gmail.com>
2019-07-25 12:09:00 -0700

    fix minor styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,44 @@
 import React, { Component, Fragment } from "react";
+import Nav from "./Nav";
+import don from "../assets/don.png";
+import leo from "../assets/leo.png";
+import mike from "../assets/mike.png";
+import ralph from "../assets/ralph.png";
 
 class About extends Component {
   state = {};
   render() {
     return (
       <Fragment>
-        <h3>The team</h3>
-        <p>hello</p>
+        <Nav />
+        <div className="about">
+          <ul>
+            <li>
+              <a href="https://github.com/sapanapoudel">
+                <img src={don} />
+              </a>
+              <p>Sapana</p>
+            </li>
+            <li>
+              <a href="https://github.com/JBusch2010">
+                <img src={leo} />
+              </a>
+              <p>Joachen</p>
+            </li>
+            <li>
+              <a href="https://github.com/Stevengrant">
+                <img src={mike} />
+              </a>
+              <p>Steve</p>
+            </li>
+            <li>
+              <a href="https://github.com/bomibear">
+                <img src={ralph} />
+              </a>
+              <p>BomiBear</p>
+            </li>
+          </ul>
+        </div>
       </Fragment>
     );
   }
